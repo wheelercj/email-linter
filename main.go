bc402123147a4c702a8c42022ef648e7187159f2
wheelercj <chrisw289@gmail.com>
2023-12-08 01:49:40 -0800

    Move general JMAP functions from main.go to new file client.go

--- main.go
+++ main.go
@@ -15,11 +15,7 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
 	"fmt"
-	"io"
-	"net/http"
 	"os"
 	"slices"
 	"strings"
@@ -43,7 +39,7 @@ func main() {
 	printAddresses(singleUseAddresses, toAndFrom)
 }
 
-// getApiToken looks for a JMAP_TOKEN environment variable, and asks for the token to be
+// getApiToken looks for a JMAP_TOKEN environment variable, or asks for the token to be
 // entered interactively as a fallback.
 func getApiToken() string {
 	token := os.Getenv("JMAP_TOKEN")
@@ -58,76 +54,6 @@ func getApiToken() string {
 		}
 	}
 	return token
-}
-
-// getApiSession makes a web request to create an API session.
-func getApiSession(token string) map[string]any {
-	sessionRes, err := makeJmapCall("GET", ApiSessionUrl, token, "")
-	if err != nil {
-		panic(err)
-	}
-	sessionBytes, err := io.ReadAll(sessionRes.Body)
-	if err != nil {
-		panic(err)
-	}
-	if strings.EqualFold(string(sessionBytes), "Authorization header not a valid format\n") {
-		panic("Authorization header in an invalid format or has an invalid token")
-	}
-	var session map[string]any
-	err = json.Unmarshal(sessionBytes, &session)
-	if err != nil {
-		panic(err)
-	}
-	return session
-}
-
-// getAccountIdAndApiUrl makes a web request to get the user's account ID and the API
-// session URL to use for all further web requests.
-func getAccountIdAndApiUrl(token string) (string, string) {
-	session := getApiSession(token)
-	primaryAccounts := session["primaryAccounts"].(map[string]any)
-	accountId := primaryAccounts["urn:ietf:params:jmap:mail"].(string)
-	if verbose {
-		fmt.Printf("account ID: %s\n", accountId)
-	}
-	url := session["apiUrl"].(string)
-	return accountId, url
-}
-
-// getInboxEmailsRecipients makes a web request for the names and addresses in the "to"
-// fields of all emails in the inbox.
-func getInboxEmailsRecipients(inboxId, accountId, url, token string) []any {
-	emailsReqBody := fmt.Sprintf(`
-		{
-			"using": ["urn:ietf:params:jmap:core", "urn:ietf:params:jmap:mail"],
-			"methodCalls": [
-				[
-					"Email/query",
-					{
-						"accountId": "%s",
-						"filter": {"inMailbox": "%s"}
-					},
-					"0"
-				],
-				[
-					"Email/get",
-					{
-						"accountId": "%s",
-						"#ids": {
-							"resultOf": "0",
-							"name": "Email/query",
-							"path": "/ids"
-						},
-						"properties": ["to"]
-					},
-					"1"
-				]
-			]
-		}
-	`, accountId, inboxId, accountId)
-
-	emailsList := getEmailsList(emailsReqBody, url, token)
-	return emailsList
 }
 
 // getSingleUseAddresses makes a web request for emails in the inbox, and from them
@@ -172,7 +98,7 @@ func getSendersToSingleUseAddresses(
 	singleUseAddresses []string, spamId, accountId, url, token string,
 ) map[string][]string {
 	singleUseAddressesStr := strings.Join(singleUseAddresses, "\"}, {\"to\": \"")
-	emailsReq2Body := fmt.Sprintf(`
+	emailsReqBody := fmt.Sprintf(`
 		{
 			"using": ["urn:ietf:params:jmap:core", "urn:ietf:params:jmap:mail"],
 			"methodCalls": [
@@ -212,10 +138,10 @@ func getSendersToSingleUseAddresses(
 		}
 	`, accountId, spamId, singleUseAddressesStr, accountId)
 
-	emailsList2 := getEmailsList(emailsReq2Body, url, token)
+	emailsList := getEmailsList(emailsReqBody, url, token)
 
 	toAndFrom := make(map[string][]string)
-	for _, emailAny := range emailsList2 {
+	for _, emailAny := range emailsList {
 		email := emailAny.(map[string]any)
 		to := strings.ToLower(email["to"].([]any)[0].(map[string]any)["email"].(string))
 		from := strings.ToLower(email["from"].([]any)[0].(map[string]any)["email"].(string))
@@ -271,127 +197,4 @@ func appendIfSingleUse(singleUseAddresses []string, emailDataList []any) []strin
 		singleUseAddresses = append(singleUseAddresses, address)
 	}
 	return singleUseAddresses
-}
-
-// getInboxAndSpamIds makes a web request for the IDs of the inbox and the spam folder.
-func getInboxAndSpamIds(accountId, url, token string) (string, string) {
-	mailboxesReqBody := fmt.Sprintf(`
-		{
-			"using": ["urn:ietf:params:jmap:core", "urn:ietf:params:jmap:mail"],
-			"methodCalls": [
-				[
-					"Mailbox/query",
-					{
-						"accountId": "%s",
-						"filter": {
-							"operator": "OR",
-							"conditions": [
-								{"role": "inbox"},
-								{"name": "spam"}
-							]
-						}
-					},
-					"0"
-				],
-				[
-					"Mailbox/get",
-					{
-						"accountId": "%s",
-						"#ids": {
-							"resultOf": "0",
-							"name": "Mailbox/query",
-							"path": "/ids"
-						},
-						"properties": ["id", "role", "name"]
-					},
-					"1"
-				]
-			]
-		}
-	`, accountId, accountId)
-	mailboxesRes, err := makeJmapCall("POST", url, token, mailboxesReqBody)
-	if err != nil {
-		panic(err)
-	}
-	mailboxesBytes, err := io.ReadAll(mailboxesRes.Body)
-	if err != nil {
-		panic(err)
-	}
-	if bytes.Equal(mailboxesBytes, []byte("Malformed JSON")) {
-		panic("Malformed JSON")
-	}
-	var mailboxes map[string]any
-	err = json.Unmarshal(mailboxesBytes, &mailboxes)
-	if err != nil {
-		panic(err)
-	}
-	mailboxesMethodRes := mailboxes["methodResponses"].([]any)
-	inboxAndSpam := mailboxesMethodRes[1].([]any)[1].(map[string]any)["list"].([]any)
-
-	var inboxId, spamId string
-	ufo := inboxAndSpam[0].(map[string]any) // unidentified folder object
-	otherUfo := inboxAndSpam[1].(map[string]any)
-	ufoName := strings.ToLower(ufo["name"].(string))
-	ufoRole := strings.ToLower(ufo["role"].(string))
-	if ufoName == "inbox" || ufoRole == "inbox" {
-		inboxId = ufo["id"].(string)
-		spamId = otherUfo["id"].(string)
-	} else {
-		inboxId = otherUfo["id"].(string)
-		spamId = ufo["id"].(string)
-	}
-
-	if verbose {
-		fmt.Printf("inbox ID: %s\n", inboxId)
-		fmt.Printf("spam folder ID: %s\n", spamId)
-	}
-
-	return inboxId, spamId
-}
-
-// getEmailsList makes a web request with a JMAP API request body, the API's url, and an
-// API token to get an array of email data objects from a JMAP server. The expected JMAP
-// methods are "Email/query" followed by "Email/get" (two methods total).
-func getEmailsList(emailsReqBody, url, token string) []any {
-	emailsRes, err := makeJmapCall("POST", url, token, emailsReqBody)
-	if err != nil {
-		panic(err)
-	}
-	emailsBytes, err := io.ReadAll(emailsRes.Body)
-	if err != nil {
-		panic(err)
-	}
-	if bytes.Equal(emailsBytes, []byte("Malformed JSON")) {
-		panic("Malformed JSON")
-	}
-
-	var emails map[string]any
-	err = json.Unmarshal(emailsBytes, &emails)
-	if err != nil {
-		panic(err)
-	}
-	emailsMethodRes := emails["methodResponses"].([]any)
-	emailsGetRes := emailsMethodRes[1].([]any)
-	emailsMap := emailsGetRes[1].(map[string]any)
-	emailsList := emailsMap["list"].([]any)
-	return emailsList
-}
-
-// makeJmapCall makes a web request with content type "application/json" using the
-// default http client. If the given body string is empty, nil is sent as the body.
-func makeJmapCall(httpMethod, url, token, body string) (*http.Response, error) {
-	var req *http.Request
-	var err error
-	if len(body) > 0 {
-		bodyReader := bytes.NewReader([]byte(body))
-		req, err = http.NewRequest(httpMethod, url, bodyReader)
-	} else {
-		req, err = http.NewRequest(httpMethod, url, nil)
-	}
-	if err != nil {
-		return nil, err
-	}
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", token))
-	return http.DefaultClient.Do(req)
 }
